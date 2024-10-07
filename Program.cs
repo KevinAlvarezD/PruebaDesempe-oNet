e6b9e4c6902c1ad1ffd5a2098aad4c19e8b92b9d
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-10-06 20:46:30 -0500

    Feat: Modify program to Connect with database

--- Program.cs
+++ Program.cs
@@ -8,7 +8,26 @@ using System.Text.Json.Serialization;
 using PruebaDempeño.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+Env.Load();
+builder.Configuration.AddEnvironmentVariables();
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var mySqlConnection = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.Parse("8.0.20-mysql")));
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        options.JsonSerializerOptions.WriteIndented = true;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
