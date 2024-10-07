3bac5dbaa5e2111f1102f1757442921955a80acf
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-10-06 21:16:14 -0500

    Feat: UserDelete Endpoint was created

---
+++ Controllers/V1/Users/UsersDeleteController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaDempeño.Controllers.V1.Users
+{
+    public partial class UsersController : ControllerBase
+    {
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userRepository.CheckExistence(id);
+            if (user == false)
+            {
+                return NotFound();
+            }
+
+            await _userRepository.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
