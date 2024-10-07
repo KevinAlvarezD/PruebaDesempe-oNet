4d2c8d1d6af5803760842621288b30acf300878c
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-10-06 21:20:31 -0500

    Feat: Create UserUpdateController

---
+++ Controllers/V1/Users/UsersUpdateController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using PruebaDempeño.Helpers;
+
+using PruebaDempeño.DTOs.Request;
+
+namespace PruebaDempeño.Controllers.V1.Users
+{
+    public partial class UsersController : ControllerBase
+    {
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UserDTO updatedUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var checkUser = await _userRepository.CheckExistence(id);
+            if (checkUser == false)
+            {
+                return NotFound();
+            }
+
+            var user = await _userRepository.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Username = updatedUser.Username;
+            user.Email = updatedUser.Email;
+            user.Password = updatedUser.Password = PasswordHasher.HashPassword(updatedUser.Password);
+            user.Role = updatedUser.Role;
+
+
+            await _userRepository.Update(user);
+            return NoContent();
+        }
+    }
+
+
+}
