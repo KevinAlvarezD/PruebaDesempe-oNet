a1d84d20375f8af40948e7f1babe1aa45fe1413a
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-10-06 20:39:04 -0500

    Feat: Create DTO User

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TechStore.Models;
 
 namespace PruebaDempeño.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
 
 }
